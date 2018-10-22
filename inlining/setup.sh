2c87af31b961adfe46c933de0b26cb498a17341e
renato <renato.grottesi@gmail.com>
2018-10-22 19:44:54 +0200

    removing the inlining example as it got embedded in the slides

--- inlining/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yotta init
-yotta target frdm-k64f-gcc
-yotta target bbc-microbit-classic-gcc
-yotta install lancaster-university/microbit
-yotta build
-ls build/bbc-microbit-classic-gcc/source/*-combined.hex 
